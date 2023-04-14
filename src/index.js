4b2d14fbab178bd1a8191ea48e59bbd5d3022760
caroumelo <caroumelo1405@gmail.com>
2023-04-13 22:19:52 -0300

    TesteFetchJS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@
 //         const listPerson = document.querySelector("#list-p")
 //     })
 // }  
+/*
+opção 1:
 const doGet = (url) => {
     const promiseCallback = (resolve, reject) => {
         fetch(url)
@@ -20,3 +22,47 @@ const doGet = (url) => {
     return new Promise(promiseCallback); 
 }
 doGet('https://rickandmortyapi.com/api/character').then(console.log).catch(console.error);
+*/
+
+/*opção 2: const response = fetch('https://rickandmortyapi.com/api/character')
+    .then(function(responseData){
+        return responseData.json();
+    })
+    .then(function(jsonData){
+        console.log(jsonData);
+    })
+    .catch(function(e){
+        console.log('DEU ERRO');
+    })
+*/
+
+/* opção 3:
+
+(async function(){
+    try{
+        
+        const headers = {
+             'Content-Type': 'application/json',
+            'authoriization': 'Bearer asdlfo; akjfkajf',
+        };
+        const init = {
+            method: 'POST',//eu poderia mundar para PUT (atualizar). Se fosse o patch, só alteraria o campo. 
+            //se for Delete, apago meu id criado e não precisaria mais do body.
+            headers: headers,
+            body: JSON.stringify({
+                title: 'Testando',
+            }),
+        };
+        const response = await fetch('https://rickandmortyapi.com/api/character', 
+        init);
+
+        const jsonData = await response.json();
+    console.log(jsonData);
+
+     
+    } catch(e){
+        console.log('DEU ERRO!');
+    }
+    
+})();
+*/
