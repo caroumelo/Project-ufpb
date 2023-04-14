873dac2f65847614129866a10b5db9c2384ca21d
Jayanne Morais <jayannelcm@gmail.com>
2023-04-13 22:25:24 -0300

    updates for fetch

--- src/index.js
+++ src/index.js
@@ -1,22 +1,42 @@
-// let url = "https://rickandmortyapi.com/api/character"
+let url = "https://rickandmortyapi.com/api/character"
 
-// function fetchApi(url){
-//     fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         const listPerson = document.querySelector("#list-p")
-//     })
-// }  
-const doGet = (url) => {
-    const promiseCallback = (resolve, reject) => {
-        fetch(url)
-        .then((response) => {
-            if(!response.ok) throw new Error('Erro ao executar requisição, status' + response.status);
-            return response.json();
+function fetchApi(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const list = document.querySelector('list-cards')
+        data.map((item)=>{
+
+            const li = document.createElement('li');
+            li.setAttribute('id',item.id);
+            li.innerHTML = item.name;
+            list.appendChild(li);
         })
-        .then(resolve)
-        .catch(reject);     
-    }
-    return new Promise(promiseCallback); 
-}
-doGet('https://rickandmortyapi.com/api/character').then(console.log).catch(console.error);
+    }).catch(erro=> console.log(erro))
+   
+}  
+fetchApi(url);
+// const doGet = (url) => {
+//     const promiseCallback = (resolve, reject) => {
+//         fetch(url)
+//         .then((response) => {
+//             if(!response.ok) throw new Error('Erro ao executar requisição, status' + response.status);
+//             return response.json();
+//         })
+//         .then(resolve)
+//         .catch(reject);     
+//     }
+//     return new Promise(promiseCallback); 
+// }
+// doGet('https://rickandmortyapi.com/api/character').then(
+//     data=> {
+//         const list = document.querySelector('list-cards')
+//         data.map((item)=>{
+
+//             const li = document.createElement('li');
+//             li.setAttribute('id',item.id);
+//             li.innerHTML = item.name;
+//             list.appendChild(li);
+//         })
+//     }
+// ).catch(console.error);
