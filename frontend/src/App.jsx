9583214e803a53dbae2b83d12a2e12d632d76f0b
Jayanne Morais <jayannelcm@gmail.com>
2023-05-14 12:40:02 -0300

    conectando o front ao back

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+
+import React from "react";
+import Container from "./components/Container/Container";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+
+    <>
+    <div className="container-fluid">
+    
+    <Header/>
+    <div></div>
+
+    <Container/>
+    </div>
+ 
+     
+      
+    </>
+    
+
+   
+      
+    
+  );
+}
+
+export default App;
